38d18a258d274e9a40fc155e1d9797f4acdce00a
rafael.fumegalli <rafaelxxc@gmail.com>
2022-06-27 18:16:41 -0300

    Ecommerce Project Part 1

---
+++ class-06/ecommerce/src/OrderItem.ts
@@ -0,0 +1,12 @@
+export default class OrderItem {
+
+    constructor (
+        readonly id: number,
+        readonly price: number,
+        readonly quantity: number,
+    ) {}
+
+    getTotal () {
+        return this.price * this.quantity;
+    }
+}
---
+++ class-06/ecommerce/test/Order.test.ts
@@ -0,0 +1,26 @@
+import Coupon from "../src/Coupon";
+import Item from "../src/Item";
+import Order from "../src/Order";
+
+test('should not create an order with invalid cpf', () => {
+    expect(() => new Order('111.111.111-11')).toThrow(new Error('CPF Inválido'));
+});
+
+test('should create an order with 3 items (description, price and quantity)', () => {
+    const order = new Order('935.411.347-80');
+    order.addItem(new Item(1, 'Panela', 100), 1);
+    order.addItem(new Item(2, 'Garfolher', 15), 2);
+    order.addItem(new Item(3, 'Faca', 5), 2);
+    const total = order.getTotal();
+    expect(total).toBe(140);
+});
+
+test('should create an order with coupon (discount from total)', () => {
+    const order = new Order('935.411.347-80');
+    order.addItem(new Item(1, 'Panela', 100), 1);
+    order.addItem(new Item(2, 'Garfolher', 15), 2);
+    order.addItem(new Item(3, 'Faca', 5), 2);
+    order.addCoupon(new Coupon('VALE20', 20));
+    const total = order.getTotal();
+    expect(total).toBe(112);
+});
